137f2ddbe4c5be73c795a59a57122073344df9de
Surya Kumara <suryakumara33@gmail.com>
2021-06-06 15:35:32 +0800

    revise readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import Navbar from './component/Navbar';
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
-import Home from './component/pages/Home';
-import Services from './component/pages/Services';
-import Products from './component/pages/Products';
-import SignUp from './component/pages/SignUp';
+import React from "react";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./component/pages/Home";
+import Services from "./component/pages/Services";
+import SignUp from "./component/pages/SignUp";
+import About from "./component/pages/About";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" exact component={Home} />
+          <Route path="/booking-terms" component={Services} />
+          <Route path="/about-us" component={About} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
